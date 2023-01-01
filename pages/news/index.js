dbaae6d74880f6e6af33e32a5cf714e001809c97
MyuB <mjycoco0@naver.com>
2023-01-01 16:16:30 +0900

    Link for SPA in nextJS

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,18 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/first-page">first</Link>
+        </li>
+        <li>second</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
